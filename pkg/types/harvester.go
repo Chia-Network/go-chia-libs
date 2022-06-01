ea9040552b434549c784105fb09fac28c7a62a23
Chris Marslender <chrismarslender@gmail.com>
2022-06-01 10:58:54 -0500

    Add farming info event

---
+++ pkg/types/harvester.go
@@ -0,0 +1,10 @@
+package types
+
+// EventHarvesterFarmingInfo is the event data for `farming_info` from the harvester
+type EventHarvesterFarmingInfo struct {
+	ChallengeHash string  `json:"challenge_hash"`
+	TotalPlots    uint64  `json:"total_plots"`
+	FoundProofs   uint64  `json:"found_proofs"`
+	EligiblePlots uint64  `json:"eligible_plots"`
+	Time          float64 `json:"found_time"`
+}
