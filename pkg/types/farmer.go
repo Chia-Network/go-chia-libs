61b3f3f821277d82e7035ed780f53fd5092fd84f
Chris Marslender <chrismarslender@gmail.com>
2022-06-01 11:07:46 -0500

    Farmer proof event

--- pkg/types/farmer.go
+++ pkg/types/farmer.go
@@ -7,3 +7,23 @@ type EventFarmerSubmittedPartial struct {
 	CurrentDifficulty            uint64 `json:"current_difficulty"`
 	PointsAcknowledgedSinceStart uint64 `json:"points_acknowledged_since_start"`
 }
+
+// EventFarmerProof is the farmer event `proof`
+type EventFarmerProof struct {
+	Proof        DeclareProofOfSpace `json:"proof"`
+	PassedFilter bool                `json:"passed_filter"`
+}
+
+// DeclareProofOfSpace matches to the farmer protocol type
+type DeclareProofOfSpace struct {
+	ChallengeHash             string       `json:"challenge_hash"`
+	ChallengeChainSP          string       `json:"challenge_chain_sp"`
+	SignagePointIndex         uint8        `json:"signage_point_index"`
+	RewardChainSP             string       `json:"reward_chain_sp"`
+	ProofOfSpace              ProofOfSpace `json:"proof_of_space"`
+	ChallengeChainSPSignature G2Element    `json:"challenge_chain_sp_signature"`
+	RewardChainSPSignature    G2Element    `json:"reward_chain_sp_signature"`
+	FarmerPuzzleHash          string       `json:"farmer_puzzle_hash"`
+	PoolTarget                *PoolTarget  `json:"pool_target,omitempty"`
+	PoolSignature             *G2Element   `json:"pool_signature,omitempty"`
+}
