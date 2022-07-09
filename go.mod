a109e212ac4de301eff6254e3d664599a42069c0
Chris Marslender <chrismarslender@gmail.com>
2022-07-08 20:32:40 -0500

    Fix ipv6 parsing when address is wrapped by [] in the rpc

--- go.mod
+++ go.mod
@@ -2,10 +2,16 @@ module github.com/chia-network/go-chia-libs
 
 go 1.17
 
-require gopkg.in/yaml.v2 v2.4.0
+require (
+	github.com/google/go-querystring v1.1.0
+	github.com/gorilla/websocket v1.5.0
+	github.com/patrickmn/go-cache v2.1.0+incompatible
+	github.com/stretchr/testify v1.8.0
+	gopkg.in/yaml.v2 v2.4.0
+)
 
 require (
-	github.com/google/go-querystring v1.1.0 // indirect
-	github.com/gorilla/websocket v1.5.0 // indirect
-	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pkg/types/connections.go
+++ pkg/types/connections.go
@@ -1,6 +1,10 @@
 package types
 
-import "net"
+import (
+	"encoding/json"
+	"net"
+	"strings"
+)
 
 // NodeType is the type of peer (farmer, full node, etc)
 type NodeType uint8
@@ -25,19 +29,40 @@ const (
 	NodeTypeWallet NodeType = 6
 )
 
+// IPAddress wraps net.IP so we can fix unmarshalling issues
+// due to invalid [] wrapping ipv6 addresses in Chia RPC
+type IPAddress struct {
+	net.IP
+}
+
 // Connection represents a single peer or internal connection
 type Connection struct {
 	BytesRead    uint64 `json:"bytes_read"`
 	BytesWritten uint64 `json:"bytes_written"`
 	//CreationTime // @TODO parse to time - is seconds as float
 	//LastMessageTime // @TODO parse to time - is seconds as float
-	LocalPort      uint16   `json:"local_port"`
-	NodeID         string   `json:"node_id"`
-	PeakHash       string   `json:"peak_hash"`
-	PeakHeight     uint32   `json:"peak_height"`
-	PeakWeight     Uint128  `json:"peak_weight"`
-	PeerHost       net.IP   `json:"peer_host"`
-	PeerPort       uint16   `json:"peer_port"`
-	PeerServerPort uint16   `json:"peer_server_port"`
-	Type           NodeType `json:"type"`
+	LocalPort      uint16    `json:"local_port"`
+	NodeID         string    `json:"node_id"`
+	PeakHash       string    `json:"peak_hash"`
+	PeakHeight     uint32    `json:"peak_height"`
+	PeakWeight     Uint128   `json:"peak_weight"`
+	PeerHost       IPAddress `json:"peer_host"`
+	PeerPort       uint16    `json:"peer_port"`
+	PeerServerPort uint16    `json:"peer_server_port"`
+	Type           NodeType  `json:"type"`
+}
+
+// UnmarshalJSON Unmarshals IP
+func (ip *IPAddress) UnmarshalJSON(data []byte) error {
+	strData := strings.ReplaceAll(string(data), "[", "")
+	strData = strings.ReplaceAll(strData, "]", "")
+
+	ipval := &net.IP{}
+	err := json.Unmarshal([]byte(strData), ipval)
+	if err != nil {
+		return err
+	}
+
+	ip.IP = *ipval
+	return nil
 }
