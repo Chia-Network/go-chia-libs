cb28f3d1d2c09ff8f16402b20557ee31e3e5909a
Starttoaster <b.butler@chia.net>
2022-09-11 21:57:08 -0700

    Fix some types

--- pkg/types/nft.go
+++ pkg/types/nft.go
@@ -7,17 +7,17 @@ type NFT struct {
 	LauncherID         string   `json:"launcher_id"`
 	LauncherPuzhash    string   `json:"launcher_puzhash"`
 	LicenseHash        string   `json:"license_hash"`
-	LicenseUris        []string `json:"license_uris"`
+	LicenseURIs        []string `json:"license_uris"`
 	MetadataHash       string   `json:"metadata_hash"`
-	MetadataUris       []string `json:"metadata_uris"`
-	MintHeight         int      `json:"mint_height"`
+	MetadataURIs       []string `json:"metadata_uris"`
+	MintHeight         uint32   `json:"mint_height"`
 	NftCoinID          string   `json:"nft_coin_id"`
 	OwnerDid           string   `json:"owner_did"`
 	PendingTransaction bool     `json:"pending_transaction"`
-	RoyaltyPercentage  int      `json:"royalty_percentage"`
+	RoyaltyPercentage  uint32   `json:"royalty_percentage"`
 	RoyaltyPuzzleHash  string   `json:"royalty_puzzle_hash"`
-	EditionNumber      int      `json:"edition_number"`
-	EditionCount       int      `json:"edition_count"`
+	EditionNumber      uint32   `json:"edition_number"`
+	EditionCount       uint32   `json:"edition_count"`
 	SupportsDid        bool     `json:"supports_did"`
 	UpdaterPuzhash     string   `json:"updater_puzhash"`
 }
