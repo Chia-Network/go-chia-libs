0a236eef390de75392895fbdabab9167fe3cffdb
Chris Marslender <chrismarslender@gmail.com>
2022-11-09 11:04:18 -0600

    Turns out the RPCs ACTUALLY just expect memos to be a list of strings, and converts to bytes on that end. no support for passing in raw bytes now

--- pkg/types/spend.go
+++ pkg/types/spend.go
@@ -2,7 +2,7 @@ package types
 
 // Addition an addition for a spend
 type Addition struct {
-	Amount     uint64  `json:"amount"`
-	PuzzleHash Bytes32 `json:"puzzle_hash"`
-	Memos      []Bytes `json:"memos,omitempty"`
+	Amount     uint64   `json:"amount"`
+	PuzzleHash Bytes32  `json:"puzzle_hash"`
+	Memos      []string `json:"memos,omitempty"`
 }
