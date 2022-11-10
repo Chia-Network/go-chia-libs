3149fa2a1d2117b54665c4c9188dcf9b75f6509d
Chris Marslender <chrismarslender@gmail.com>
2022-11-09 22:31:00 -0600

    Validate harvester.go types

--- pkg/types/harvester.go
+++ pkg/types/harvester.go
@@ -1,6 +1,11 @@
 package types
 
+import (
+	"github.com/samber/mo"
+)
+
 // EventHarvesterFarmingInfo is the event data for `farming_info` from the harvester
+// https://github.com/Chia-Network/chia-blockchain/blob/main/chia/harvester/harvester_api.py#L232
 type EventHarvesterFarmingInfo struct {
 	ChallengeHash Bytes32 `json:"challenge_hash"`
 	TotalPlots    uint64  `json:"total_plots"`
@@ -10,13 +15,15 @@ type EventHarvesterFarmingInfo struct {
 }
 
 // PlotInfo contains information about a plot, as used in get_plots rpc
+// There is also a PlotInfo type in chia, that is NOT used in the RPC, that has the first 5 fields as defined here
+// https://github.com/Chia-Network/chia-blockchain/blob/main/chia/harvester/harvester.py#L139
 type PlotInfo struct {
-	FileSize               uint64     `json:"file_size"`
-	Filename               string     `json:"filename"`
-	PlotID                 Bytes32    `json:"plot_id"`
-	PlotPublicKey          G1Element  `json:"plot_public_key"`
-	PoolContractPuzzleHash *Bytes32   `json:"pool_contract_puzzle_hash"`
-	PoolPublicKey          *G1Element `json:"pool_public_key"`
-	Size                   uint8      `json:"size"`
-	TimeModified           int        `json:"time_modified"`
+	PoolPublicKey          mo.Option[G1Element] `json:"pool_public_key"`
+	PoolContractPuzzleHash mo.Option[Bytes32]   `json:"pool_contract_puzzle_hash"`
+	PlotPublicKey          G1Element            `json:"plot_public_key"`
+	FileSize               uint64               `json:"file_size"`
+	TimeModified           Timestamp            `json:"time_modified"`
+	Filename               string               `json:"filename"`
+	PlotID                 Bytes32              `json:"plot_id"`
+	Size                   uint8                `json:"size"` // https://github.com/Chia-Network/chiapos/blob/main/src/prover_disk.hpp#L181
 }
