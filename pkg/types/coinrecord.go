0cdc8ca558fa6f3c160aab1a01275c9d8766e3f3
Chris Marslender <chrismarslender@gmail.com>
2022-11-09 22:12:24 -0600

    Coinrecord verification/source documentation

--- pkg/types/coinrecord.go
+++ pkg/types/coinrecord.go
@@ -1,10 +1,12 @@
 package types
 
 // CoinRecord type
+// https://github.com/Chia-Network/chia-blockchain/blob/main/chia/types/coin_record.py#L15
+// @TODO Streamable
 type CoinRecord struct {
-	Coin                Coin   `json:"coin"`
-	ConfirmedBlockIndex uint32 `json:"confirmed_block_index"`
-	SpentBlockIndex     uint32 `json:"spent_block_index"`
-	Coinbase            bool   `json:"coinbase"`
-	Timestamp           uint64 `json:"timestamp"`
+	Coin                Coin      `json:"coin"`
+	ConfirmedBlockIndex uint32    `json:"confirmed_block_index"`
+	SpentBlockIndex     uint32    `json:"spent_block_index"`
+	Coinbase            bool      `json:"coinbase"`
+	Timestamp           Timestamp `json:"timestamp"`
 }
