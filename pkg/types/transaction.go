cfaab249612fadf2ca4f5867b1be28d76e45c7e7
Chris Marslender <chrismarslender@gmail.com>
2022-11-09 23:04:43 -0600

    Validate transaction.go

--- pkg/types/transaction.go
+++ pkg/types/transaction.go
@@ -3,26 +3,31 @@ package types
 import (
 	"encoding/json"
 	"fmt"
+
+	"github.com/samber/mo"
 )
 
 // TransactionRecord Single Transaction
+// https://github.com/Chia-Network/chia-blockchain/blob/main/chia/wallet/transaction_record.py#L26
+// @TODO Streamable
 type TransactionRecord struct {
-	ConfirmedAtHeight uint32           `json:"confirmed_at_height"`
-	CreatedAtTime     uint64           `json:"created_at_time"` // @TODO time.Time?
-	ToPuzzleHash      *Bytes32         `json:"to_puzzle_hash"`
-	Amount            uint64           `json:"amount"`
-	FeeAmount         uint64           `json:"fee_amount"`
-	Confirmed         bool             `json:"confirmed"`
-	Sent              uint32           `json:"sent"`
-	SpendBundle       *SpendBundle     `json:"spend_bundle"`
-	Additions         []*Coin          `json:"additions"`
-	Removals          []*Coin          `json:"removals"`
-	WalletID          uint32           `json:"wallet_id"`
-	SentTo            []*SentTo        `json:"sent_to"`
-	TradeID           *Bytes32         `json:"trade_id"`
-	Type              *TransactionType `json:"type"`
-	Name              Bytes32          `json:"name"`
-	// ToAddress is not on the official type, but some endpoints return it anyways
+	ConfirmedAtHeight uint32                 `json:"confirmed_at_height"`
+	CreatedAtTime     Timestamp              `json:"created_at_time"`
+	ToPuzzleHash      Bytes32                `json:"to_puzzle_hash"`
+	Amount            uint64                 `json:"amount"`
+	FeeAmount         uint64                 `json:"fee_amount"`
+	Confirmed         bool                   `json:"confirmed"`
+	Sent              uint32                 `json:"sent"`
+	SpendBundle       mo.Option[SpendBundle] `json:"spend_bundle"`
+	Additions         []Coin                 `json:"additions"`
+	Removals          []Coin                 `json:"removals"`
+	WalletID          uint32                 `json:"wallet_id"`
+	SentTo            []SentTo               `json:"sent_to"` // List[Tuple[str, uint8, Optional[str]]]
+	TradeID           mo.Option[Bytes32]     `json:"trade_id"`
+	Type              TransactionType        `json:"type"`
+	Name              Bytes32                `json:"name"`
+	//Memos             []MemoTuple            `json:"memos"` // List[Tuple[bytes32, List[bytes]]]
+	// ToAddress is not on the official type, but some endpoints return it anyways. This part is not streamable
 	ToAddress string `json:"to_address"`
 }
 
@@ -31,8 +36,8 @@ type TransactionRecord struct {
 // sent_to: List[Tuple[str, uint8, Optional[str]]]
 type SentTo struct {
 	Peer                   string
-	MempoolInclusionStatus *MempoolInclusionStatus
-	Error                  *string
+	MempoolInclusionStatus MempoolInclusionStatus
+	Error                  mo.Option[string]
 }
 
 // UnmarshalJSON unmarshals the SentTo tuple into the struct
@@ -50,6 +55,7 @@ func (s *SentTo) UnmarshalJSON(buf []byte) error {
 }
 
 // MempoolInclusionStatus status of being included in the mempool
+// https://github.com/Chia-Network/chia-blockchain/blob/main/chia/types/mempool_inclusion_status.py#L6
 type MempoolInclusionStatus uint8
 
 const (
@@ -64,6 +70,7 @@ const (
 )
 
 // TransactionType type of transaction
+// https://github.com/Chia-Network/chia-blockchain/blob/main/chia/wallet/util/transaction_type.py#L6
 type TransactionType uint32
 
 const (
@@ -86,8 +93,10 @@ const (
 	TransactionTypeOutgoingTrade TransactionType = 5
 )
 
-// SpendBundle Spend Bundle...
+// SpendBundle Spend Bundle
+// https://github.com/Chia-Network/chia-blockchain/blob/main/chia/types/spend_bundle.py#L20
+// @TODO Streamable
 type SpendBundle struct {
-	CoinSpends          []*CoinSpend `json:"coin_spends"`
-	AggregatedSignature G2Element    `json:"aggregated_signature"`
+	CoinSpends          []CoinSpend `json:"coin_spends"`
+	AggregatedSignature G2Element   `json:"aggregated_signature"`
 }
