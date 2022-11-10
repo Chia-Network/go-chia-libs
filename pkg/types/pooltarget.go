4cf50c223e54cd7bda8e0621f93b4e9ecf060412
Chris Marslender <chrismarslender@gmail.com>
2022-11-09 22:38:39 -0600

    Validate pooltarget.go

--- pkg/types/pooltarget.go
+++ pkg/types/pooltarget.go
@@ -1,7 +1,9 @@
 package types
 
 // PoolTarget PoolTarget
+// https://github.com/Chia-Network/chia-blockchain/blob/main/chia/types/blockchain_format/pool_target.py#L12
+// @TODO Streamable
 type PoolTarget struct {
-	PuzzleHash *Bytes32 `json:"puzzle_hash"`
-	MaxHeight  uint32   `json:"max_height"`
+	PuzzleHash Bytes32 `json:"puzzle_hash"`
+	MaxHeight  uint32  `json:"max_height"`
 }
