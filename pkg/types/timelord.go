224871fa56f2dc63516f9250844234b9d077cf6c
Chris Marslender <chrismarslender@gmail.com>
2022-11-09 22:57:53 -0600

    validate timelord.go types

--- pkg/types/timelord.go
+++ pkg/types/timelord.go
@@ -1,18 +1,21 @@
 package types
 
 // SkippingPeakEvent data that is sent when a timelord skips a peak because it was fastest/already had the peak
+// https://github.com/Chia-Network/chia-blockchain/blob/main/chia/timelord/timelord_api.py#L44
 type SkippingPeakEvent struct {
 	Success bool   `json:"success"`
 	Height  uint32 `json:"height"`
 }
 
 // NewPeakEvent data that is sent when a timelord skips a peak because it was fastest/already had the peak
+// https://github.com/Chia-Network/chia-blockchain/blob/main/chia/timelord/timelord_api.py#L49
 type NewPeakEvent struct {
 	Success bool   `json:"success"`
 	Height  uint32 `json:"height"`
 }
 
 // TimelordChain references a particular chain within timelord code
+// https://github.com/Chia-Network/chia-blockchain/blob/main/chia/timelord/types.py#L6
 type TimelordChain uint8
 
 const (
@@ -30,16 +33,18 @@ const (
 )
 
 // FinishedPoTEvent data every time a PoT Challenge is completed
+// https://github.com/Chia-Network/chia-blockchain/blob/main/chia/timelord/timelord.py#L1050
 type FinishedPoTEvent struct {
 	Success          bool          `json:"success"`
-	Chain            TimelordChain `json:"chain"`
 	EstimatedIPS     float64       `json:"estimated_ips"`
 	IterationsNeeded uint64        `json:"iterations_needed"`
+	Chain            TimelordChain `json:"chain"`
 	VDFInfo          VDFInfo       `json:"vdf_info"`
 	VDFProof         VDFProof      `json:"vdf_proof"`
 }
 
 // CompressibleVDFField Stores, for a given VDF, the field that uses it.
+// https://github.com/Chia-Network/chia-blockchain/blob/main/chia/types/blockchain_format/vdf.py#L94
 type CompressibleVDFField uint8
 
 const (
@@ -57,6 +62,7 @@ const (
 )
 
 // NewCompactProofEvent is an event from the timelord every time a new compact proof is generated
+// https://github.com/Chia-Network/chia-blockchain/blob/main/chia/timelord/timelord.py#L1074
 type NewCompactProofEvent struct {
 	Success    bool                 `json:"success"`
 	HeaderHash Bytes32              `json:"header_hash"`
