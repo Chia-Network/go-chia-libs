ebb006143a1b34325e49e1f39d2bf8f24a2eefcd
Chris Marslender <chrismarslender@gmail.com>
2022-11-09 22:04:45 -0600

    Add marshalling/unmarshalling for the custom timestamp type

--- pkg/types/timestamp.go
+++ pkg/types/timestamp.go
@@ -1,11 +1,27 @@
 package types
 
 import (
+	"fmt"
+	"strconv"
 	"time"
 )
 
 // Timestamp Helper type to go to/from uint64 timestamps in json but represent as time.Time in go applications
-type Timestamp time.Time
+type Timestamp struct {
+	time.Time
+}
 
-// @TODO Unmarshal
-// @TODO Marshal
+// MarshalJSON marshals the time to unix timestamp
+func (t Timestamp) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf("%d", t.Time.Unix())), nil
+}
+
+// UnmarshalJSON unmarshals from uint64
+func (t *Timestamp) UnmarshalJSON(data []byte) error {
+	intval, err := strconv.Atoi(string(data))
+	if err != nil {
+		return err
+	}
+	t.Time = time.Unix(int64(intval), 0)
+	return nil
+}
