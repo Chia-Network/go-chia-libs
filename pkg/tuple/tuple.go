0f59e25e160e32bc483596ce7f89d18bb533ad50
Chris Marslender <chrismarslender@gmail.com>
2022-11-10 21:20:51 -0600

    Implement a much more sane strategy than a giant switch

--- pkg/tuple/tuple.go
+++ pkg/tuple/tuple.go
@@ -4,8 +4,6 @@ import (
 	"bytes"
 	"encoding/json"
 	"reflect"
-
-	"github.com/samber/mo"
 )
 
 // Some builds a Tupe when value is present.
@@ -36,63 +34,26 @@ func (t Tuple[T]) MarshalJSON() ([]byte, error) {
 }
 
 // UnmarshalJSON decodes Option from json.
+// This works by creating a temp []interface{}, where each item in the slice is a pointer to the fields of the underlying
+// tuple struct. Since it's pre-filled with pointers, unmarshal fills the pointers and checks the types
 func (t *Tuple[T]) UnmarshalJSON(b []byte) error {
 	if bytes.Equal(b, []byte("null")) {
 		t.isPresent = false
 		return nil
 	}
 
-	var tmpMap []interface{}
+	v := reflect.ValueOf(&t.value).Elem()
+	tmpMap := make([]interface{}, v.NumField())
+	tmv := reflect.ValueOf(&tmpMap).Elem()
+	for i := 0; i < v.NumField(); i++ {
+		field := v.Field(i)
+		tmv.Index(i).Set(field.Addr())
+	}
 	err := json.Unmarshal(b, &tmpMap)
 	if err != nil {
 		return err
 	}
 
-	vp := reflect.ValueOf(&t.value)
-	v := vp.Elem()
-	for i := 0; i < v.NumField(); i++ {
-		field := v.Field(i)
-		setType(&field, tmpMap[i])
-	}
-
 	t.isPresent = true
 	return nil
-}
-
-func setType(field *reflect.Value, fillValue interface{}) {
-	if fillValue == nil {
-		return
-	}
-	switch field.Kind() {
-	case reflect.Bool:
-	case reflect.Int:
-	case reflect.Int8:
-	case reflect.Int16:
-	case reflect.Int32:
-	case reflect.Int64:
-	case reflect.Uint:
-	case reflect.Uint8:
-		// numeric looking things seem to unmarshal generically into float64
-		if floated, ok := fillValue.(float64); ok {
-			field.SetUint(uint64(floated))
-		}
-	case reflect.Uint16:
-	case reflect.Uint32:
-	case reflect.Uint64:
-	case reflect.String:
-		if stringed, ok := fillValue.(string); ok {
-			field.SetString(stringed)
-		}
-	case reflect.Struct:
-		// The only nested struct we support for now is Option[T]
-		actualType := field.Type().Name()
-		switch actualType {
-		case "Option[string]":
-			os := mo.Some(fillValue.(string))
-			osrp := reflect.ValueOf(&os)
-			osr := osrp.Elem()
-			field.Set(osr)
-		}
-		// @TODO handle other types
-	}
 }
