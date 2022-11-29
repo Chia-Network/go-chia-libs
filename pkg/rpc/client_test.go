8915cc1b688f6fd81e2df3b33125df78339a2359
Starttoaster <b.butler@chia.net>
2022-11-29 10:51:52 -0800

    Remove port options, update tests

--- pkg/rpc/client_test.go
+++ pkg/rpc/client_test.go
@@ -1,18 +1,22 @@
 package rpc
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
 	"os"
 	"strconv"
 	"testing"
+
+	"github.com/chia-network/go-chia-libs/pkg/config"
 )
 
 func setup(t *testing.T) (*http.ServeMux, *httptest.Server, *Client) {
 	mux := http.NewServeMux()
 	server := httptest.NewTLSServer(mux)
 
+	// Get port from server
 	u, err := url.Parse(server.URL)
 	if err != nil {
 		t.Fatal(err)
@@ -21,14 +25,25 @@ func setup(t *testing.T) (*http.ServeMux, *httptest.Server, *Client) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	// Get user home dir for CHIA_ROOT
+	home, err := os.UserHomeDir()
+	if err != nil {
+		t.Fatal(err)
+	}
 	client, err := NewClient(ConnectionModeHTTP,
-		WithAutoConfig(),
-		WithDaemonPort(uint16(p)),
-		WithNodePort(uint16(p)),
-		WithFarmerPort(uint16(p)),
-		WithHarvesterPort(uint16(p)),
-		WithWalletPort(uint16(p)),
-		WithCrawlerPort(uint16(p)))
+		WithManualConfig(config.ChiaConfig{
+			ChiaRoot: fmt.Sprintf("%s/.chia/mainnet", home),
+			Wallet: config.WalletConfig{
+				PortConfig: config.PortConfig{
+					RPCPort: uint16(p),
+				},
+				SSL: config.SSLConfig{
+					PrivateCRT: "config/ssl/wallet/private_wallet.crt",
+					PrivateKey: "config/ssl/wallet/private_wallet.key",
+				},
+			},
+		}))
 	if err != nil {
 		t.Fatal(err)
 	}
