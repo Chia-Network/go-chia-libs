590cfec82156e0af3870f0bac436cd439fde96dc
Chris Marslender <chrismarslender@gmail.com>
2023-06-28 11:06:05 -0500

    Add full node protocol with request_peers for now

---
+++ pkg/peerprotocol/fullnode.go
@@ -0,0 +1,22 @@
+package peerprotocol
+
+import (
+	"github.com/chia-network/go-chia-libs/pkg/protocols"
+)
+
+// FullNodeProtocol is for interfacing with full nodes via the peer protocol
+type FullNodeProtocol struct {
+	connection *Connection
+}
+
+// NewFullNodeProtocol returns a new instance of the full node protocol
+func NewFullNodeProtocol(connection *Connection) (*FullNodeProtocol, error) {
+	fnp := &FullNodeProtocol{connection: connection}
+
+	return fnp, nil
+}
+
+// RequestPeers asks the current peer to respond with their current peer list
+func (c *FullNodeProtocol) RequestPeers() error {
+	return c.connection.Do(protocols.ProtocolMessageTypeRequestPeers, &protocols.RequestPeers{})
+}
