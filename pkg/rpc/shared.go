17aa6635d051c396a98e65736e731b11ea35be2c
Chris Marslender <chrismarslender@gmail.com>
2024-03-06 10:49:33 -0600

    NetworkName/Prefix are optional

--- pkg/rpc/shared.go
+++ pkg/rpc/shared.go
@@ -1,11 +1,15 @@
 package rpc
 
+import (
+	"github.com/samber/mo"
+)
+
 // GetNetworkInfoOptions options for the get_network_info rpc calls
 type GetNetworkInfoOptions struct {}
 
 // GetNetworkInfoResponse common get_network_info response from all RPC services
 type GetNetworkInfoResponse struct {
 	Response
-	NetworkName   string `json:"network_name"`
-	NetworkPrefix string `json:"network_prefix"`
+	NetworkName   mo.Option[string] `json:"network_name"`
+	NetworkPrefix mo.Option[string] `json:"network_prefix"`
 }
