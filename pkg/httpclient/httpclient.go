2f916b85aba8fcd5412eca7f0f1986c61734476a
Chris Marslender <chrismarslender@gmail.com>
2024-03-16 22:25:30 -0500

    Add logs to http client

--- pkg/httpclient/httpclient.go
+++ pkg/httpclient/httpclient.go
@@ -413,7 +413,11 @@ func (c *HTTPClient) AddDisconnectHandler(onDisconnect rpcinterface.DisconnectHa
 func (c *HTTPClient) AddReconnectHandler(onReconnect rpcinterface.ReconnectHandler) {}
 
 // SetSyncMode does not apply to the HTTP Client
-func (c *HTTPClient) SetSyncMode() {}
+func (c *HTTPClient) SetSyncMode() {
+	c.logger.Debug("Sync mode is default for HTTP client")
+}
 
 // SetAsyncMode does not apply to the HTTP Client
-func (c *HTTPClient) SetAsyncMode() {}
+func (c *HTTPClient) SetAsyncMode() {
+	c.logger.Debug("Async mode is not applicable to the HTTP client. SetAsyncMode call is ignored")
+}
