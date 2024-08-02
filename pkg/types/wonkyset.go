0ca92af45d0840b19e840a3d0e474f95dd9946e4
Chris Marslender <chrismarslender@gmail.com>
2024-08-02 14:05:43 -0500

    Return a better error when wonkyset can't be unmarshalled

--- pkg/types/wonkyset.go
+++ pkg/types/wonkyset.go
@@ -1,5 +1,9 @@
 package types
 
+import (
+	"fmt"
+)
+
 // WonkySet is just an alias for map[string]string that will unmarshal correctly from the empty forms {} and []
 // In chia-blockchain, the default for some sets was incorrectly [] in the initial config
 // so this ensures compatability with both ways the empty values could show up
@@ -8,16 +12,18 @@ type WonkySet map[string]string
 // UnmarshalYAML implements the yaml.Unmarshaler interface.
 func (ws *WonkySet) UnmarshalYAML(unmarshal func(interface{}) error) error {
 	// Attempt to unmarshal into a slice of strings.
+	var sliceErr error
+	var mapErr error
 	var slice []string
-	if err := unmarshal(&slice); err == nil {
+	if sliceErr = unmarshal(&slice); sliceErr == nil {
 		*ws = make(map[string]string)
 		return nil
 	}
 
 	// Attempt to unmarshal into a map.
 	var m map[string]string
-	if err := unmarshal(&m); err != nil {
-		return err
+	if mapErr = unmarshal(&m); mapErr != nil {
+		return fmt.Errorf("failed to unmarshal as either string slice of map: slice err: %s | map err: %s", sliceErr.Error(), mapErr.Error())
 	}
 
 	*ws = m
