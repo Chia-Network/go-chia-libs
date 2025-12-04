29913399192580724bb038a615fd2d8a44077eb3
Chris Marslender <chrismarslender@gmail.com>
2025-12-03 22:32:02 -0600

    Support both PKCS1 and PKCS8 private key formats (#223)
    * Support both PKCS1 and PKCS8 privatte key formats
    Old chia versions used PKCS1
    
    * Remove redundant break

--- pkg/tls/tls_test.go
+++ pkg/tls/tls_test.go
@@ -1,10 +1,11 @@
 package tls
 
 import (
-	"crypto/rsa"
 	"crypto/x509"
-	"encoding/pem"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 var testCertPEMBlock = []byte(`-----BEGIN CERTIFICATE-----
@@ -57,33 +58,132 @@ H7ij3Gb3zSd+ziUMa7WxDCe8kBA5delHjzy8m8UrhmijpMJIOKk/5EwPEyCUnREZ
 Rb7RT3n8oEZxwPDlbW3OAVBH
 -----END PRIVATE KEY-----`)
 
+var testOldCertPEMBlock = []byte(`-----BEGIN CERTIFICATE-----
+MIIDyzCCArOgAwIBAgIUXIpxI5MoZQ65/vhc7DK/d5ymoMUwDQYJKoZIhvcNAQEL
+BQAwRDENMAsGA1UECgwEQ2hpYTEQMA4GA1UEAwwHQ2hpYSBDQTEhMB8GA1UECwwY
+T3JnYW5pYyBGYXJtaW5nIERpdmlzaW9uMB4XDTI1MTExOTIxMDg1MFoXDTM3MTIz
+MTIxMDg1MFowRDENMAsGA1UECgwEQ2hpYTEQMA4GA1UEAwwHQ2hpYSBDQTEhMB8G
+A1UECwwYT3JnYW5pYyBGYXJtaW5nIERpdmlzaW9uMIIBIjANBgkqhkiG9w0BAQEF
+AAOCAQ8AMIIBCgKCAQEAzz/L219Zjb5CIKnUkpd2julGC+j3E97KUiuOalCH9wdq
+gpJi9nBqLccwPCSFXFew6CNBIBM+CW2jT3UVwgzjdXJ7pgtu8gWj0NQ6NqSLiXV2
+WbpZovfrVh3x7Z4bjPgI3ouWjyehUfmK1GPIld4BfUSQtPlUJ53+XT32GRizUy+b
+0CcJ84jp1XvyZAMajYnclFRNNJSw9WXtTlMUu+Z1M4K7c4ZPwEqgEnCgRc0TCaXj
+180vo7mCHJQoDiNSCRATwfH+kWxOOK/nePkq2t4mPSFaX8xAS4yILISIOWYn7sNg
+dy9D6gGNFo2SZ0FR3x9hjUjYEV3cPqg3BmNE3DDynQIDAQABo4G0MIGxMA8GA1Ud
+EwEB/wQFMAMBAf8wHQYDVR0OBBYEFD4KTvuce45Yfu6qWASlekSwUBZaMH8GA1Ud
+IwR4MHaAFD4KTvuce45Yfu6qWASlekSwUBZaoUikRjBEMQ0wCwYDVQQKDARDaGlh
+MRAwDgYDVQQDDAdDaGlhIENBMSEwHwYDVQQLDBhPcmdhbmljIEZhcm1pbmcgRGl2
+aXNpb26CFFyKcSOTKGUOuf74XOwyv3ecpqDFMA0GCSqGSIb3DQEBCwUAA4IBAQAI
+vF2RAVyN5H5BU/OK6icEw8SGGfoaqpSMCQRuSwz9qg3QLDQj8yd5FYs9VECuigdw
+i+mVnycT3vAObx+KfsPFlyUpw3RJ0+F53aDy+tW6ykTU94GQeLF5LN528NYLo2vt
+YZ/4cGW+wjf+a+qBYjd3bW6HI9QGdJ1/BQhfwxexy4T+x4cleB++/vUFj6ZM1WhM
+XRRTC3QzFBOCmTY4y21Mxvh5dOwY9HxK2qvwiO45zRWQ8Z0V95y4rytRqiDbNvFG
+Jbs9Iwm8nj6c0Gm6rLkFNZ6OaYhX/5KhX35xE7YYIdorKwi+EKqhDD1jAwDA3q6T
+Z4wumksKcqmUZXw9+pYX
+-----END CERTIFICATE-----`)
+
+var testOldKeyPEMBlock = []byte(`-----BEGIN RSA PRIVATE KEY-----
+MIIEowIBAAKCAQEAzz/L219Zjb5CIKnUkpd2julGC+j3E97KUiuOalCH9wdqgpJi
+9nBqLccwPCSFXFew6CNBIBM+CW2jT3UVwgzjdXJ7pgtu8gWj0NQ6NqSLiXV2WbpZ
+ovfrVh3x7Z4bjPgI3ouWjyehUfmK1GPIld4BfUSQtPlUJ53+XT32GRizUy+b0CcJ
+84jp1XvyZAMajYnclFRNNJSw9WXtTlMUu+Z1M4K7c4ZPwEqgEnCgRc0TCaXj180v
+o7mCHJQoDiNSCRATwfH+kWxOOK/nePkq2t4mPSFaX8xAS4yILISIOWYn7sNgdy9D
+6gGNFo2SZ0FR3x9hjUjYEV3cPqg3BmNE3DDynQIDAQABAoIBAGupS4BJdx8gEAAh
+2VDRqAAzhHTZb8j9uoKXJ+NotEkKrDTqUMiOu0nOqOsFWdYPo9HjxoggFuEU+Hpl
+a4kj4uF3OG6Yj+jgLypjpV4PeoFM6M9R9BCp07In2i7DLLK9gvYA85SoVLBd/tW4
+hFH+Qy3M+ZNZ1nLCK4pKjtaYs0dpi5zLoVvpEcEem2O+aRpUPCZqkNwU0umATCfg
+ZGfFzgXI/XPJr8Uy+LVZOFp3PXXHfnZZD9T5AjO/ViBeqbMFuWQ8BpVOqapNPKj8
+xDY3ovw3uiAYPC7eLib3u/WoFelMc2OMX0QljLp5Y+FScFHAMxoco3AQdWSYvSQw
+b5xZmg0CgYEA6zKASfrw3EtPthkLR5NBmesI4RbbY6iFVhS5loLbzTtStvsus8EI
+6RQgLgAFF14H21YSHxb6dB1Mbo45BN83gmDpUvKPREslqD3YPMKFo5GXMmv+JhNo
+5Y9fhiOEnxzLJGtBB1HeGmg5NXp9mr2Ch9u8w/slfuCHckbA9AYvdxMCgYEA4ZR5
+zg73+UA1a6Pm93bLYZGj+hf7OaB/6Hiw9YxCBgDfWM9dJ48iz382nojT5ui0rClV
+5YAo8UCLh01Np9AbBZHuBdYm9IziuKNzTeK31UW+Tvbz+dEx7+PlYQffNOhcIgd+
+9SXjoZorQksImKdMGZld1lEReHuBawq92JQvtY8CgYEAtNwUws7xQLW5CjKf9d5K
+5+1Q2qYU9sG0JsmxHQhrtZoUtRjahOe/zlvnkvf48ksgh43cSYQF/Bw7lhhPyGtN
+6DhVs69KdB3FS2ajTbXXxjxCpEdfHDB4zW4+6ouNhD1ECTFgxBw0SuIye+lBhSiN
+o6NZuOr7nmFSRpIZ9ox7G3kCgYA4pvxMNtAqJekEpn4cChab42LGLX2nhFp7PMxc
+bqQqM8/j0vg3Nihs6isCd6SYKjstvZfX8m7V3/rquQxWp9oRdQvNJXJVGojaDBqq
+JdU7V6+qzzSIufQLpjV2P+7br7trxGwrDx/y9vAETynShLmE+FJrv6Jems3u3xy8
+psKwmwKBgG5uLzCyMvMB2KwI+f3np2LYVGG0Pl1jq6yNXSaBosAiF0y+IgUjtWY5
+EejO8oPWcb9AbqgPtrWaiJi17KiKv4Oyba5+y36IEtyjolWt0AB6F3oDK0X+Etw8
+j/xlvBNuzDL6gRJHQg1+d4dO8Lz54NDUbKW8jGl+N/7afGVpGmX9
+-----END RSA PRIVATE KEY-----`)
+
 func TestGenerateAllCerts_ProvidedCA(t *testing.T) {
-	// Decode the PEM-encoded certificate to get DER format
-	certBlock, _ := pem.Decode(testCertPEMBlock)
-	if certBlock == nil {
-		t.Fatal("Failed to decode PEM block containing certificate")
-	}
-	// Convert DER to x509 Certificate
-	cert, err := x509.ParseCertificate(certBlock.Bytes)
+	cert, err := ParsePemCertificate(testCertPEMBlock)
+	require.NoError(t, err)
 	if err != nil {
 		t.Fatalf("Failed to parse certificate: %v", err)
 	}
 
 	// Decode the PEM-encoded private key
-	keyBlock, _ := pem.Decode(testKeyPEMBlock)
-	if keyBlock == nil {
-		t.Fatal("Failed to decode PEM block containing private key")
-	}
-	// Parse the private key in PKCS#8 format
-	parsedKey, err := x509.ParsePKCS8PrivateKey(keyBlock.Bytes)
+	privateKey, err := ParsePemKey(testKeyPEMBlock)
+	require.NoError(t, err)
+
+	allCerts, err := GenerateAllCerts(cert, privateKey)
 	if err != nil {
-		t.Fatalf("Failed to parse private key: %v", err)
+		t.Fatalf("Failed to generate all certificates: %v", err)
 	}
-	// Type assert to RSA private key
-	privateKey, ok := parsedKey.(*rsa.PrivateKey)
-	if !ok {
-		t.Fatal("Parsed key is not an RSA private key")
+
+	// Test that private CA cert and private key match from the GenerateAllCerts output
+	privateCACert, err := x509.ParseCertificate(allCerts.PrivateCA.CertificateDER)
+	if err != nil {
+		t.Fatalf("Failed to parse private CA certificate: %v", err)
 	}
+	if !CertMatchesPrivateKey(privateCACert, allCerts.PrivateCA.PrivateKey) {
+		t.Fatal("Private CA certificate and private key do not match")
+	}
+
+	// Loop through all public certificate-key pairs and verify that the pair is not nil, that the key matches the certificate
+	for _, nodeHelpers := range publicNodes {
+		crtKey := nodeHelpers.fetch(allCerts)
+
+		// Test pair is not nil
+		if crtKey == nil || crtKey.PrivateKey == nil || crtKey.CertificateDER == nil || len(crtKey.CertificateDER) == 0 {
+			t.Fatalf("Certificate pair, or one of the certificate or key is empty : %s", nodeHelpers.certKeyBase)
+		}
+
+		// Test pair matches
+		cert, err := x509.ParseCertificate(crtKey.CertificateDER)
+		if err != nil {
+			t.Fatalf("Failed to parse certificate for %s: %v", nodeHelpers.certKeyBase, err)
+		}
+		if !CertMatchesPrivateKey(cert, crtKey.PrivateKey) {
+			t.Fatalf("certificate and private key do not match for %s", nodeHelpers.certKeyBase)
+		}
+	}
+
+	// Loop through all private certificate-key pairs and verify that the pair is not nil, that the key matches the certificate
+	for _, nodeHelpers := range privateNodes {
+		crtKey := nodeHelpers.fetch(allCerts)
+
+		// Test pair is not nil
+		if crtKey == nil || crtKey.PrivateKey == nil || crtKey.CertificateDER == nil || len(crtKey.CertificateDER) == 0 {
+			t.Fatalf("Certificate pair, or one of the certificate or key is empty : %s", nodeHelpers.certKeyBase)
+		}
+
+		// Test pair matches
+		cert, err := x509.ParseCertificate(crtKey.CertificateDER)
+		if err != nil {
+			t.Fatalf("Failed to parse certificate for %s: %v", nodeHelpers.certKeyBase, err)
+		}
+		if !CertMatchesPrivateKey(cert, crtKey.PrivateKey) {
+			t.Fatalf("certificate and private key do not match for %s", nodeHelpers.certKeyBase)
+		}
+	}
+}
+
+func TestGenerateAllCerts_ProvidedOldCA(t *testing.T) {
+	cert, err := ParsePemCertificate(testOldCertPEMBlock)
+	require.NoError(t, err)
+	if err != nil {
+		t.Fatalf("Failed to parse certificate: %v", err)
+	}
+
+	// Decode the PEM-encoded private key
+	privateKey, err := ParsePemKey(testOldKeyPEMBlock)
+	require.NoError(t, err)
 
 	allCerts, err := GenerateAllCerts(cert, privateKey)
 	if err != nil {
@@ -190,4 +290,18 @@ func TestGenerateAllCerts_GeneratedCA(t *testing.T) {
 			t.Fatalf("certificate and private key do not match for %s", nodeHelpers.certKeyBase)
 		}
 	}
+}
+
+func TestParsePemKeyNewFormat(t *testing.T) {
+	key, err := ParsePemKey(testKeyPEMBlock)
+	require.NoError(t, err)
+	err = key.Validate()
+	assert.NoError(t, err)
+}
+
+func TestParsePemKeyOldFormat(t *testing.T) {
+	key, err := ParsePemKey(testOldKeyPEMBlock)
+	require.NoError(t, err)
+	err = key.Validate()
+	assert.NoError(t, err)
 }
