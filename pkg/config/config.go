96769cba52595318c531ba9df8e653077dab73b8
Chris Marslender <chrismarslender@gmail.com>
2026-02-02 15:14:37 -0600

    Update initial config based on 2.6.0-rc

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -158,7 +158,7 @@ type NetworkConstants struct {
 	MempoolBlockBuffer             uint8          `yaml:"MEMPOOL_BLOCK_BUFFER,omitempty" json:"MEMPOOL_BLOCK_BUFFER,omitempty"`
 	MinPlotSize                    uint8          `yaml:"MIN_PLOT_SIZE,omitempty" json:"MIN_PLOT_SIZE,omitempty"`
 	MinPlotSizeV1                  uint8          `yaml:"MIN_PLOT_SIZE_V1,omitempty" json:"MIN_PLOT_SIZE_V1,omitempty"`
-	MinPlotSizeV2                  uint8          `yaml:"MIN_PLOT_SIZE_V2,omitempty" json:"MIN_PLOT_SIZE_V2,omitempty"`
+	PlotSizeV2                     uint8          `yaml:"PLOT_SIZE_V2,omitempty" json:"PLOT_SIZE_V2,omitempty"`
 	NetworkType                    uint8          `yaml:"NETWORK_TYPE,omitempty" json:"NETWORK_TYPE,omitempty"`
 	SubSlotItersStarting           uint64         `yaml:"SUB_SLOT_ITERS_STARTING,omitempty" json:"SUB_SLOT_ITERS_STARTING,omitempty"`
 	// All pointers that that 0 is an allowed value when marshaling with omitempty, but they will still be omitted from configs that dont have them defined
@@ -166,6 +166,7 @@ type NetworkConstants struct {
 	SoftFork4Height     *uint32 `yaml:"SOFT_FORK4_HEIGHT,omitempty" json:"SOFT_FORK4_HEIGHT,omitempty"`
 	SoftFork5Height     *uint32 `yaml:"SOFT_FORK5_HEIGHT,omitempty" json:"SOFT_FORK5_HEIGHT,omitempty"`
 	SoftFork6Height     *uint32 `yaml:"SOFT_FORK6_HEIGHT,omitempty" json:"SOFT_FORK6_HEIGHT,omitempty"`
+	SoftFork8Height     *uint32 `yaml:"SOFT_FORK8_HEIGHT,omitempty" json:"SOFT_FORK8_HEIGHT,omitempty"`
 	PlotFilter128Height *uint32 `yaml:"PLOT_FILTER_128_HEIGHT,omitempty" json:"PLOT_FILTER_128_HEIGHT,omitempty"`
 	PlotFilter64Height  *uint32 `yaml:"PLOT_FILTER_64_HEIGHT,omitempty" json:"PLOT_FILTER_64_HEIGHT,omitempty"`
 	PlotFilter32Height  *uint32 `yaml:"PLOT_FILTER_32_HEIGHT,omitempty" json:"PLOT_FILTER_32_HEIGHT,omitempty"`
